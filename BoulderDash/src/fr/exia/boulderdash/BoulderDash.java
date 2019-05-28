309be7900b7a307135d799a6e6114f228a059714
singe <singe@LAPTOP-15K6OV8B>
2019-05-28 16:18:57 +0200

    Interaction BDD + utilisation des maps

--- BoulderDash/src/fr/exia/boulderdash/BoulderDash.java
+++ BoulderDash/src/fr/exia/boulderdash/BoulderDash.java
@@ -2,10 +2,10 @@ package fr.exia.boulderdash;
 
 import java.io.IOException;
 
-import fr.exia.boulderdash.controller.IBoulderDashController;
 import fr.exia.boulderdash.controller.BoulderDashController;
-import fr.exia.boulderdash.model.IBoulderDashModel;
+import fr.exia.boulderdash.controller.IBoulderDashController;
 import fr.exia.boulderdash.model.BoulderDashModel;
+import fr.exia.boulderdash.model.IBoulderDashModel;
 import fr.exia.boulderdash.view.BoulderDashView;
 
 public abstract class BoulderDash {
@@ -15,7 +15,7 @@ public abstract class BoulderDash {
     private static final int startY = 3;
 
     public static void main(final String[] args) throws IOException, InterruptedException {
-        final IBoulderDashModel model = new BoulderDashModel("Map3.txt", startX, startY);
+        final IBoulderDashModel model = new BoulderDashModel("Map.txt", startX, startY);
         final BoulderDashView view = new BoulderDashView(model.getMap(), model.getMyHero());
         final IBoulderDashController controller = new BoulderDashController(view, model);
         view.setOrderPerformer(controller.getOrderPeformer());
--- BoulderDash/src/fr/exia/boulderdash/model/Map.java
+++ BoulderDash/src/fr/exia/boulderdash/model/Map.java
@@ -4,6 +4,10 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.sql.CallableStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Observable;
 
 import fr.exia.boulderdash.model.element.IElement;
@@ -12,17 +16,46 @@ import fr.exia.boulderdash.model.element.motionless.MotionlessElementsFactory;
 class Map extends Observable implements IMap {
 
     private int          width;
-
     private int          height;
-
     private IElement[][] onTheMap;
-
+    public static String Map = "";
+	public static int i = 0;
+	
     Map(final String fileName) throws IOException {
         super();
         this.loadFile(fileName);
     }
 
     private void loadFile(final String fileName) throws IOException {
+    	
+
+    		final String sql = "{call SelectionMap(?)}";
+    		CallableStatement call;
+    		try {
+    			call = DBConnection.getInstance().getConnection().prepareCall(sql);
+    			call.setInt(1, 1);
+    			call.execute();
+    			ResultSet resultSet = call.getResultSet();
+    	
+    			while (resultSet.next()) {
+    				Map = Map + resultSet.getString("Objet");
+    				i = i + 1;
+    				if(i==20) {
+    					i = 0;
+    					Map = Map +"\r\n";
+    				}
+    				PrintWriter writer = new PrintWriter("C:\\Users\\singe\\git\\Projet-Java\\BoulderDash\\Map.txt", "UTF-8");
+    				writer.println("20\r\n20");
+    				writer.println(Map);
+    				writer.close();
+    				
+    		}
+    			}
+    		
+    		catch (SQLException e) {
+    			e.printStackTrace();
+    		}
+    	
         final BufferedReader buffer = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
         String line;
         int y = 0;
